fc2f2d2aec5f9f7b43d337dcaec0125e92b14f3f
Ehis Ojielu <eojielu@gmail.com>
2022-09-01 15:14:18 -0500

    September updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,16 +9,16 @@ set -x -U GOPATH $HOME/go
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 
 # make Neovim the default editor
-set --export EDITOR "nvim"
+set --export EDITOR nvim
 
 # FZF configuration
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude node_modules --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_TMUX=1 
+export FZF_TMUX=1
 export FZF_CTRL_T_OPTS="--preview 'bat {} | head -200'"
 
 function fssh -d "Fuzzy-find ssh host via ag and ssh into it"
-  ag --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
+    ag --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
 end
 
 # >>> conda initialize >>>
@@ -40,13 +40,31 @@ set -x PATH $PATH $HOME/vsco/runbooks/bin
 
 # vsco ssh keys
 function lssh -d "Load ssh keys"
-   ssh-add ~/.ssh/github_rsa
-   ssh-add ~/.ssh/vsco_rsa
+    ssh-add ~/.ssh/github_rsa
+    ssh-add ~/.ssh/vsco_rsa
+end
+
+# vsco launch browser in debug mode
+function launch_debug_chrome_browser -d "Launch Chrome in Debug mode"
+    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222
 end
 
 fish_add_path /usr/local/opt/node@16/bin
 fish_add_path /usr/local/opt/openjdk@11/bin
 fish_add_path /usr/local/opt/mysql-client/bin
 
+
+# kubectl completion for fish
+kubectl completion fish | source
+
 # https://starship.rs/guide/#%F0%9F%9A%80-installation
 starship init fish | source
+
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+# pnpm
+set -gx PNPM_HOME /Users/ehis/Library/pnpm
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
